20a3b49b8d10b3ef1e2a52793f7676d185f2c41a
Herb Stahl <ghstahl@gmail.com>
2019-07-09 04:04:39 -0700

    initial  Core needs it

---
+++ src/OIDC.MiddleMan/InMemoryIdentity/OIDCPipelineSigninManager.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using OIDC.ReferenceWebClient.Data;
+using OIDCPipeline.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OIDC.ReferenceWebClient.InMemoryIdentity
+{
+    public class OIDCPipelineSigninManager: ISigninManager
+    {
+        private SignInManager<ApplicationUser> _signInManager;
+
+        public OIDCPipelineSigninManager(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public Task SignOutAsync()
+        {
+            return _signInManager.SignOutAsync();
+        }
+    }
+}
